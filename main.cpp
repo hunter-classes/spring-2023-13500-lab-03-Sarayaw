5130c75c9e5f7011de3620d3df57da700a2e9e56
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-15 23:25:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <string>
 
-double get_min_east() {
+std::string compare_basins(std::string date) {
   std::ifstream fin("Current_Reservoir_Levels.tsv");
     if (fin.fail()) {
         std::cerr << "File cannot be opened for reading." << std::endl;
@@ -28,55 +28,31 @@ double get_min_east() {
 
   fin >> east_storage;
 
-  double min_east = east_storage;
+  double west_storage = 0.0;
   while (fin >> date) {
-    fin >> east_storage;
-    if (east_storage < min_east) {
-      min_east = east_storage;
-    }
+    fin >> west_storage;
   }
 
-  return min_east;
-}
-
-double get_max_east() {
-  std::ifstream fin("Current_Reservoir_Levels.tsv");
-    if (fin.fail()) {
-        std::cerr << "File cannot be opened for reading." << std::endl;
-        exit(1); 
-    }
-
-  std::string trash;
-  getline(fin, trash);
-
-  std::string user_date;
-  std::cout << "Enter date in 2018: ";
-  std::cin >> user_date;
-
-  std::string date;
-  double east_storage = 0.0;
-
-  do {
-    fin >> date;
+  if (east_storage > west_storage) {
+    return "East";
+  } else if (east_storage < west_storage) {
+    return "West";
+  } else {
+    return "Equal";
   }
-  while (date.compare(user_date) != 0);
-
-  fin >> east_storage;
-
-  double max_east = east_storage;
-  while (fin >> date) {
-    fin >> east_storage;
-    if (east_storage > max_east) {
-      max_east = east_storage;
-    }
-  }
-
-  return max_east;
 }
 
 int main() {
-  std::cout << "The minimum East basin storage in 2018 is " << get_min_east() << std::endl;
-  std::cout << "The maximum East basin storage in 2018 is " << get_max_east() << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-01-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-02-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-03-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-04-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-05-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-06-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-07-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-08-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-09-01") << std::endl;
+  std::cout << "The comparison of East and West basin storage on " << compare_basins("2018-10-01") << std::endl;
 
   return 0;
 }
