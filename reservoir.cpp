8bc5c804c46a30d9c49322b813c4695549f81e02
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-16 22:31:07 -0500

    Update reservoir.cpp

--- reservoir.cpp
+++ reservoir.cpp
@@ -1,9 +1,44 @@
+
 #include <iostream>
-#include <cstdlib>
 #include <fstream>
+#include <cstdlib>
+#include <climits>
 #include <string>
 
-void reverse_order(std::string date1, std::string date2) {
+
+
+double get_east_storage(std::string date){
+  string fileDate;
+  double eastSt;
+  double eastEl;
+  double westSt;
+  double westEl;
+
+ 
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be read." << std::endl;
+    exit(1); 
+  }
+
+
+  std::string junk;       
+  getline(fin, junk); 
+
+ 
+  while(fin >> fileDate >> eastSt >> eastEl >> westSt >> westEl) {
+
+   fin.ignore(INT_MAX, '\n');
+   if (fileDate == date){
+      return eastSt;
+    }
+
+  }
+  fin.close();
+  return 0.0;
+ }
+
+double get_min_east() {
   std::ifstream fin("Current_Reservoir_Levels.tsv");
     if (fin.fail()) {
         std::cerr << "File cannot be opened for reading." << std::endl;
@@ -27,24 +62,48 @@ void reverse_order(std::string date1, std::string date2) {
 
   fin >> east_storage;
 
-  double west_storage = 0.0;
+  double min_east = east_storage;
   while (fin >> date) {
-    fin >> west_storage;
-  }
-
-  while (fin >> date) {
-    if (date.compare(date2) > 0) {
-      std::cout << date << " " << west_storage << std::endl;
-    } else {
-      break;
+    fin >> east_storage;
+    if (east_storage < min_east) {
+      min_east = east_storage;
     }
   }
 
+  return min_east;
+}
+
+double get_max_east() {
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); 
+    }
+
+  std::string trash;
+  getline(fin, trash);
+
+  std::string user_date;
+  std::cout << "Enter date in 2018: ";
+  std::cin >> user_date;
+
+  std::string date;
+  double east_storage = 0.0;
+
+  do {
+    fin >> date;
+  }
+  while (date.compare(user_date) != 0);
+
+  fin >> east_storage;
+
+  double max_east = east_storage;
   while (fin >> date) {
-    if (date.compare(date1) > 0) {
-      std::cout << date << " " << west_storage << std::endl;
-    } else {
-      break;
+    fin >> east_storage;
+    if (east_storage > max_east) {
+      max_east = east_storage;
     }
   }
+
+  return max_east;
 }
